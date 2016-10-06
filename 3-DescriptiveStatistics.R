1616dd3a30ce63b1734cd3eb1fce97021cd8db1e
Ken Wilcox <wilcoxk@medamerica.com>
2016-10-06 12:43:22 -0600

    This is cool!
    but my head is full

---
+++ 3-DescriptiveStatistics.R
@@ -0,0 +1,46 @@
+# Load csv data (previously transformed)
+movies <- read.csv(
+  file = "Movies.csv",
+  quote = "\""
+)
+
+genres <- read.csv(
+  file = "Genres.csv",
+  quote = "\""
+)
+
+# Peek at data
+head(movies)
+head(genres)
+
+# Univariate statistics for qualitive variables
+table(movies$Rating)
+table(genres$Genre)
+
+# Univariate statistics for quantitative variables
+# Analyze the location of quantitative variable
+mean(movies$Runtime)
+median(movies$Runtime)
+which.max(table(movies$Runtime))
+
+# Analyze the spread of quantitative variable
+min(movies$Runtime)
+max(movies$Runtime)
+range(movies$Runtime)
+diff(range(movies$Runtime))
+
+# show the four equal partitions
+quantile(movies$Runtime)
+# or just a cutoff point
+quantile(movies$Runtime, 0.25)
+# or the percentile
+quantile(movies$Runtime, 0.90)
+
+# checking the spread of the data
+IQR(movies$Runtime) # 20 minutes
+
+# Inspect the variance of the data
+var(movies$Runtime)
+
+# standard deviation
+sd(movies$Runtime)
